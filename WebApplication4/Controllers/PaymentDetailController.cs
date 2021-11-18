f48dc3dc2ea6733564b8d25724c4d9dec8050ec4
ayoub-smayen <ayoubjobs.2019@gmail.com>
2021-11-18 21:41:34 +0100

    ASP.net api

---
+++ WebApplication4/Controllers/PaymentDetailController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailController : ControllerBase
+    {
+        private readonly PaymentDetailContext _context;
+
+        public PaymentDetailController(PaymentDetailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentDetail
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentDetail>>> GetPaymentDetails()
+        {
+            return await _context.PaymentDetails.ToListAsync();
+        }
+
+        [HttpPost("post")]
+        public async Task<ActionResult<PaymentDetail>> PostPaymentDetail(PaymentDetail paymentDetail)
+        {   
+            _context.PaymentDetails.Add(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentDetail", new { id = paymentDetail.PaymentDetailId }, paymentDetail);
+        }
+
+        // DELETE: api/PaymentDetail/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentDetail(int id)
+        {
+            var paymentDetail = await _context.PaymentDetails.FindAsync(id);
+            if (paymentDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.PaymentDetails.Remove(paymentDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentDetail(int id, PaymentDetail paymentDetail)
+        {
+            if (id != paymentDetail.PaymentDetailId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paymentDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+
+
+        private bool PaymentDetailExists(int id)
+        {
+            return _context.PaymentDetails.Any(e => e.PaymentDetailId == id);
+        }
+    }
+}
---
+++ WebApplication4/Models/PaymentDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication4.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
+    }
+}
